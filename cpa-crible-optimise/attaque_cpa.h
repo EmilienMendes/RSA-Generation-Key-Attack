81422c97c966ef79cc23a4a2a879722266146774
Emilien MENDES <emilienmendes59@gmail.com>
2024-12-07 11:47:10 +0100

    Ajout des fonction dans le header

--- cpa-crible-optimise/attaque_cpa.h
+++ cpa-crible-optimise/attaque_cpa.h
@@ -6,4 +6,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void afficher_tableau(Tableau v);
+void initialisation_tableau(Tableau t, unsigned int longeur, unsigned int largeur);
+void free_tableau(Tableau t);
+
+Tableau recuperation_mesure(char *fichier, unsigned int lambda);
+unsigned int poids_hamming(unsigned int nombre);
+unsigned int distingueur(Tableau m, Tableau l, int i);
+unsigned int argmax(unsigned int *liste, int taille_liste);
+void theoreme_reste_chinois(unsigned int *a, unsigned int *r, mpz_t x);
+
+void attaque_cpa(unsigned int lambda, char *trace, char *parametres, mpz_t p);
+
 #endif
