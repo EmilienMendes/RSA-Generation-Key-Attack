a856a739282fe1a94bf2ff4ee17f8943271eebc4
Emilien MENDES <emilienmendes59@gmail.com>
2025-01-04 11:22:25 +0100

    Fix erreur declaration M_PI

--- cpa-crible-optimise/gauss.h
+++ cpa-crible-optimise/gauss.h
@@ -1,7 +1,13 @@
 #ifndef __GAUSS_H__
 #define __GAUSS_H
 #include <math.h>
+// https://stackoverflow.com/questions/26065359/m-pi-flagged-as-undeclared-identifier
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
 #include <stdlib.h>
 #include <stdio.h>
+
+
 double gauss(double sigma);
 #endif
